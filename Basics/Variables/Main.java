f4005233083ad531a6a258247cf66fb79508b46f
Dhairya Shah <dtshah24022006@gmail.com>
2022-12-09 12:33:06 +0530

    Initial commit

---
+++ Basics/Variables/Main.java
@@ -0,0 +1,10 @@
+package Basics.Variables;
+
+class Main {
+    public static void main(String[] args) {
+        int age;
+
+        age = 16;
+        System.out.println("I am " + age + " years old.");
+    }
+}
